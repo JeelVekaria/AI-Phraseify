9fc8d5c77519dbf154f802f932cbebb7c175eadf
Jeel-Vekaria <jeelvekaria@gmail.com>
2024-05-29 10:51:23 -0400

    Added wallpaper

---
+++ src/component/Index.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+const Index = () => {
+    const [text, setText] = useState('')
+
+    return (
+         <div>
+            <input>
+            Enter here
+            </input>
+            {/* <h1 class="text-3xl font-bold underline">
+                Hello world!
+            </h1> */}
+
+
+        </div>
+    )
+}
+
+
+export default Index
