aba0494ed8a9f4e9028d717941cb5f387ba453ce
Jeel-Vekaria <jeelvekaria@gmail.com>
2024-05-30 12:06:21 -0400

    Added loading animation, fixed UI, Moved word counter

--- src/component/Index.jsx
+++ src/component/Index.jsx
@@ -7,10 +7,12 @@ const API_KEY = import.meta.env.VITE_API_KEY;
 const Index = () => {
   const [text, setText] = useState("");
   const [output, setOutput] = useState("");
+  const [loading, setLoading] = useState(false);
 
   // updates count on click
   const handleSubmit = async () => {
     try {
+      setLoading(true);
       const response = await axios.post(
         "https://api.ai21.com/studio/v1/paraphrase",
         { text },
@@ -22,6 +24,7 @@ const Index = () => {
       );
 
       console.log(response);
+      setLoading(false);
       setOutput(response.data.suggestions[0].text);
     } catch (error) {
       console.log(error);
@@ -32,14 +35,16 @@ const Index = () => {
     <div class="container">
       {/* Title */}
       <div>
-        <h1 className="honk"> Paraphrasing Tool</h1>
+        <h1 className="honk">AI Phraseify</h1>
       </div>
 
       {/* Input/Output boxes */}
       <div class="textbox">
         <div className="box">
           {/* input textbox */}
-          <pre className="title" id="inputBox">Input</pre>
+          <pre className="title" id="inputBox">
+            Input
+          </pre>
           <textarea
             type="text"
             name="text"
@@ -49,21 +54,47 @@ const Index = () => {
             placeholder="Type here..."
             onChange={(event) => setText(event.target.value)}
           />
-          <pre className="wordCount">Words: {text.length==0? 0 : text.trim().replace(/\s+/g,' ').split(" ").length}</pre>
+          <pre className="wordCount inputWordCount">
+            Words:{" "}
+            {text.length == 0
+              ? 0
+              : text.trim().replace(/\s+/g, " ").split(" ").length}
+          </pre>
         </div>
 
         <div className="box">
           {/* output textbox */}
-          <pre className="title output2" id="outputBox">Output</pre>
+          <pre className="title output2" id="outputBox">
+            Output
+          </pre>
           <textarea
             type="text"
             name="text"
             className="responsive-input output"
             id="outputBox"
-            value={output}
+            value={loading ? " " : output}
             placeholder="Output will display here..."
-            />
-            <pre className="wordCount">Words: {output.length==0? 0 : output.trim().replace(/\s+/g,' ').split(" ").length}</pre>
+          />
+          {loading && (
+            <div class="loadingAnimation">
+
+            <div class="spinner">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <pre>Generating...</pre>
+            </div>
+          )}
+          <pre className="wordCount">
+            Words:{" "}
+            {output.length == 0
+              ? 0
+              : output.trim().replace(/\s+/g, " ").split(" ").length}
+          </pre>
         </div>
       </div>
 
--- src/component/ParaphrasedText.jsx
+++ src/component/ParaphrasedText.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+
 const API_KEY = import.meta.env.VITE_API_KEY;
 const options2 = {
     method: "POST",
